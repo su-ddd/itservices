7221f3615c98a4107ee1ae06c865fcdf486dc726
Marco Wise <marco.wise@stanford.edu>
2014-03-25 17:10:11 -0700

    Added admin settings form, and PHP to JS variable passing

--- js/google_cse.js
+++ js/google_cse.js
@@ -1,7 +1,10 @@
-  (function() {
-    var cx = '003573755483893025830:w3hkrpntiho';
-    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-  })();
+(function($) {
+  Drupal.behaviors.its_search = {
+    attach: function(context, settings) {
+      var cx = settings.its_search.cx; 
+      var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+      gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+    }
+  };
+})(jQuery);
